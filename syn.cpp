1e3adb9a03e0e86f9609c96c9d185c1d915daf69
sangji0559 <gkgktkdwl@gmail.com>
2023-11-27 17:08:50 +0900

    syn referesh

--- syn.cpp
+++ syn.cpp
@@ -6,40 +6,30 @@
 
 using namespace std;
 struct node{
-    int key =-1;
+    int key = -1;
     int next = -1;
 };
 node table[32];
 
-int check_full(){
+int check_full(){ // 남은 bucket이 존재하는 지 
     for(int i = 16; i++){
-        if(table[i].key==-1) return i;
+        if(table[i].key==-1) return i; // 존재하면 index 리턴
     }
-    return -1;
+    return -1; // 없으면 -1 리턴
 }
 
 void insert_key(int mod, int key){
-    int next;
-    if(mod<16){
-        if(table[mod].key==-1) table[mod].key = key;
-        else{
-            if(table[mod].next!=-1){
-                next = check_full();
-                if(next!=-1){
-                    insert_key(next,key);
-                }
-                else{
-                    return;
-                }
-            }
-        }
-    }
-    
+    // mod에 따라서 저장 방법 다르게
+    // mod()
+}
+
+void remove_key(int mod, int key){
+
 }
 
 int main() {
 	fstream input("hash_cmd.txt", ios::in); // hash_cmd.txt에서 데이터 가져오기
-	fstream output("result.txt", ios::out);// result.txt에 데이터 출력
+	fstream output("result.txt", ios::out); // result.txt에 데이터 출력
 	
 	char c; // insert, remove 구분
 	int temp, mod;    
