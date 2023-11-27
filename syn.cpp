d47b6b4391c9b3e7c050664243d25f62a6501f77
sangji0559 <gkgktkdwl@gmail.com>
2023-11-27 13:11:24 +0900

    restart

--- syn.cpp
+++ syn.cpp
@@ -5,74 +5,50 @@
 #include<algorithm>
 
 using namespace std;
-const int TABLE_SIZE = 16;
-class HashTable {
-private:
-    vector<list<int>> table;
-
-public:
-    HashTable() : table(TABLE_SIZE) {}
-
-    void insert(int key) {
-        int index = hashFunction(key);
-
-        if (find(table[index].begin(), table[index].end(), key) == table[index].end()) {
-            table[index].push_back(key);
-        }
-    }
-
-
-    void remove(int key) {
-        int index = hashFunction(key);
-        
-        if (find(table[index].begin(), table[index].end(), key) == table[index].end()) {
-            table[index].remove(key);
-        }
-    }
-
-    void print(fstream& outFile) {
-        for (int i = 0; i < TABLE_SIZE; ++i) {
-            outFile << i << ":";
-            if (table[i].empty()) {
-                outFile << "-1,";
-            }
-            else {
-                for (int key : table[i]) {
-                    outFile << key << ",";
-                }
-            }
-            outFile << "\n";
-        }
-    }
-
-private:
-    int hashFunction(int key) {
-        return key % TABLE_SIZE;
-    }
+struct node{
+    int key =-1;
+    int next = -1;
 };
+node table[32];
+
+int check_full{
+    for(int i = 16; i++){
+        if(table[i].key==-1) return i;
+    }
+    return -1;
+}
+
+void insert_key(int key){
+    mod = key % 16;
+    if(table[mod].key !=-1){
+        if(check_full!=-1){
+            next = i;
+        }
+    }
+}
+
+void remove_key(int key){
+    mod = key % 16;
+}
 
 int main() {
 	fstream input("hash_cmd.txt", ios::in); // hash_cmd.txt에서 데이터 가져오기
 	fstream output("result.txt", ios::out);// result.txt에 데이터 출력
 	
 	char c; // insert, remove 구분
-	int temp;
-    HashTable ht;
-    
+	int temp;    
 	
 	while (!input.eof()) {
 		input.get(c);
 		if (c == 'i') {
 			input >> temp;
-            ht.insert(temp);
+            insert_key(temp);
 		}
 		else if (c == 'r') {
 			input >> temp;
-            ht.remove(temp);
+            remove_key(temp);
 		}
-	}
-
-    ht.print(output);
+    }
 	
 	input.close(); // 파일 닫기
 	output.close(); //파일 닫기
