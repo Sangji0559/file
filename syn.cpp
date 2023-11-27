717694b4cb71b02acaafafb5d524f8f41e8e2259
sangji0559 <gkgktkdwl@gmail.com>
2023-11-27 14:54:32 +0900

    store code

--- syn.cpp
+++ syn.cpp
@@ -11,24 +11,30 @@ struct node{
 };
 node table[32];
 
-int check_full{
+int check_full(){
     for(int i = 16; i++){
         if(table[i].key==-1) return i;
     }
     return -1;
 }
 
-void insert_key(int key){
-    mod = key % 16;
-    if(table[mod].key !=-1){
-        if(check_full!=-1){
-            next = i;
+void insert_key(int mod, int key){
+    int next;
+    if(mod<16){
+        if(table[mod].key==-1) table[mod].key = key;
+        else{
+            if(table[mod].next!=-1){
+                next = check_full();
+                if(next!=-1){
+                    insert_key(next,key);
+                }
+                else{
+                    return;
+                }
+            }
         }
     }
-}
-
-void remove_key(int key){
-    mod = key % 16;
+    
 }
 
 int main() {
@@ -36,17 +42,19 @@ int main() {
 	fstream output("result.txt", ios::out);// result.txt에 데이터 출력
 	
 	char c; // insert, remove 구분
-	int temp;    
+	int temp, mod;    
 	
 	while (!input.eof()) {
 		input.get(c);
 		if (c == 'i') {
 			input >> temp;
-            insert_key(temp);
+            mod = temp % 16;
+            insert_key(mod, temp);
 		}
 		else if (c == 'r') {
 			input >> temp;
-            remove_key(temp);
+            mod = temp % 16;
+            remove_key(mod, temp);
 		}
     }
 	
